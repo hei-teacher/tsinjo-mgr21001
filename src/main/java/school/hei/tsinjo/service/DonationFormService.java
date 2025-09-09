c9d8e4b66347e9d6f2b553e81d3ffdfeed87d50f
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-09-09 16:14:03 +0300

    feat: Donation form service for prefill feature

---
+++ src/main/java/school/hei/tsinjo/service/DonationFormService.java
@@ -0,0 +1,34 @@
+package school.hei.tsinjo.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.tsinjo.endpoint.http.model.DonationCreationForm;
+import school.hei.tsinjo.endpoint.http.model.ThEvent;
+
+@Service
+@AllArgsConstructor
+public class DonationFormService {
+
+    private final EventService eventService;
+
+    public DonationCreationForm getPrefillDonationForm(String email) {
+        var events = eventService.findAllWithPaymentResolution();
+        var thEvent = events.stream().map(ThEvent::new).toList();
+
+        var lastEvent = thEvent.stream()
+                .filter(e-> e.event().getUser().getEmail().equals(email))
+                .reduce((first, second) -> second)
+                .orElse(null);
+
+        if (lastEvent != null) {
+            return new DonationCreationForm(
+                    lastEvent.event().getUser().getFirstName(),
+                    lastEvent.event().getUser().getLastName(),
+                    ""
+            );
+        }
+        else{
+            return new DonationCreationForm("","","");
+        }
+    }
+}

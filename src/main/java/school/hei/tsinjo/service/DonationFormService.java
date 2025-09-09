ed198ace0e4e27e76c7a9af08a6c8a0ec4fc20da
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-09-09 18:50:44 +0300

    chore: format

--- src/main/java/school/hei/tsinjo/service/DonationFormService.java
+++ src/main/java/school/hei/tsinjo/service/DonationFormService.java
@@ -9,26 +9,25 @@ import school.hei.tsinjo.endpoint.http.model.ThEvent;
 @AllArgsConstructor
 public class DonationFormService {
 
-    private final EventService eventService;
+  private final EventService eventService;
 
-    public DonationCreationForm getPrefillDonationForm(String email) {
-        var events = eventService.findAllWithPaymentResolution();
-        var thEvent = events.stream().map(ThEvent::new).toList();
+  public DonationCreationForm getPrefillDonationForm(String email) {
+    var events = eventService.findAllWithPaymentResolution();
+    var thEvent = events.stream().map(ThEvent::new).toList();
 
-        var lastEvent = thEvent.stream()
-                .filter(e-> e.event().getUser().getEmail().equals(email))
-                .reduce((first, second) -> second)
-                .orElse(null);
+    var lastEvent =
+        thEvent.stream()
+            .filter(e -> e.event().getUser().getEmail().equals(email))
+            .reduce((first, second) -> second)
+            .orElse(null);
 
-        if (lastEvent != null) {
-            return new DonationCreationForm(
-                    lastEvent.event().getUser().getFirstName(),
-                    lastEvent.event().getUser().getLastName(),
-                    ""
-            );
-        }
-        else{
-            return new DonationCreationForm("","","");
-        }
+    if (lastEvent != null) {
+      return new DonationCreationForm(
+          lastEvent.event().getUser().getFirstName(),
+          lastEvent.event().getUser().getLastName(),
+          "");
+    } else {
+      return new DonationCreationForm("", "", "");
     }
+  }
 }

eeadf7ef5afa2168d2abea89ca031bc41f05dde2
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-10-03 12:32:55 +0300

    feat: appropriate exception

---
+++ src/main/java/school/hei/tsinjo/exception/VolaPaymentNullException.java
@@ -0,0 +1,7 @@
+package school.hei.tsinjo.exception;
+
+public class VolaPaymentNullException extends RuntimeException {
+  public VolaPaymentNullException(String message) {
+    super(message);
+  }
+}

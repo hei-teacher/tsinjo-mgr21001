47d9089c54729af55d09c951834ac3c262d45abc
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-10-08 14:17:15 +0300

    chore: add case Psptype is not om

--- src/main/java/school/hei/tsinjo/repository/mapper/JPaymentMapper.java
+++ src/main/java/school/hei/tsinjo/repository/mapper/JPaymentMapper.java
@@ -5,29 +5,33 @@ import static school.hei.tsinjo.model.psp.PspType.ORANGE_MONEY;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import school.hei.tsinjo.model.Payment;
+import school.hei.tsinjo.model.psp.PspType;
 import school.hei.tsinjo.repository.jpa.model.JPayment;
 
 @Slf4j
 @Component
 public class JPaymentMapper {
   public Payment toDomain(JPayment jPayment) {
-    return new Payment(
-        jPayment.getId(),
-        jPayment.getAmount(),
-        ORANGE_MONEY,
-        jPayment.getPspId(),
-        jPayment.getStatus(),
-        jPayment.getPspLastVerificationInstant(),
-        jPayment.getCreationInstant());
+    return switch (PspType.values()[0]) {
+      case ORANGE_MONEY ->
+              new Payment(
+                      jPayment.getId(),
+                      jPayment.getAmount(),
+                      ORANGE_MONEY,
+                      jPayment.getPspId(),
+                      jPayment.getStatus(),
+                      jPayment.getPspLastVerificationInstant(),
+                      jPayment.getCreationInstant());
+    };
   }
 
   public JPayment toEntity(Payment payment) {
     return new JPayment(
-        payment.id(),
-        payment.amount(),
-        payment.status(),
-        payment.pspId(),
-        payment.pspLastVerificationInstant(),
-        payment.creationInstant());
+            payment.id(),
+            payment.amount(),
+            payment.status(),
+            payment.pspId(),
+            payment.pspLastVerificationInstant(),
+            payment.creationInstant());
   }
 }

bbdd19c740851bdd413bb486b7fc1af7c1d52f37
SalomiaZK <salomia.razakarivony@gmail.com>
2026-03-20 13:18:44 +0300

    feat: add comment attribute to Event

--- src/main/java/school/hei/tsinjo/model/Help.java
+++ src/main/java/school/hei/tsinjo/model/Help.java
@@ -3,12 +3,12 @@ package school.hei.tsinjo.model;
 import java.time.Instant;
 
 public final class Help extends Event {
-  public Help(String id, Payment payment, User user, Instant creationInstant) {
-    super(id, payment, user, creationInstant);
+  public Help(String id, Payment payment, User user, Instant creationInstant, String comment) {
+    super(id, payment, user, creationInstant, comment);
   }
 
   @Override
   public Event withPayment(Payment newPayment) {
-    return new Help(id, newPayment, user, creationInstant);
+    return new Help(id, newPayment, user, creationInstant, comment);
   }
 }
